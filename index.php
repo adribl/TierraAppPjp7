e90a3887b4b582b031fb9cffa106bee9e1f25899
adribl <52063698+adribl@users.noreply.github.com>
2019-06-21 02:25:39 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es-mx">
+<head>
+    <meta charset="UTF-8">
+    <title>Evaluación de u3  Grupo XB</title>
+    <link rel="stylesheet" href="css/estilos.css"/>
+    <link rel="stylesheet" href="css/menu.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Rambla" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+</head>
+<body>
+   <section class="wrapper">
+    <header>
+      <h1 class="logo"><a href="index.html">Evaluación de u3</a></h1>
+	   <h2> Barrera Labra Adriana </h2>
+      <nav>
+	 
+      <ul>
+	  
+      <li><a href="index.html" class="current">Inicio</a></li>
+      <li><a href=basicoJS.html>Básica</a></li>
+      </ul>
+      </nav>
+    </header>
+    <section id="container">
+           <h1>Solución de problemas de Física</h1>
+
+    </section>
+    <footer class="pie">
+     Creative Commons versión 3.0 SciSoft 2019
+    </footer>
+   </section>
+</body>
+</html>
